29e489ca37935e67eb565151a51a9d75a96efe02
jaeilnet <sgsgself@naver.com>
2021-08-08 18:56:13 +0900

    leture compelte

---
+++ src/components/Movie.js
@@ -0,0 +1,47 @@
+import React from "react"
+import { Link } from "react-router-dom"
+import PropTypes from "prop-types"
+import "./Movie.css"
+
+// components
+function Movie({ id, year, title, summary, poster, genres}){
+  return (
+  <div className="movie">
+    <Link to={{
+      pathname:`/movie_detail/${id}`,
+      state:{
+        year,
+        title,
+        summary,
+        poster,
+        genres
+      }
+    }}>
+      
+        <img src={poster} alt={title} title={title}></img>
+        <div className="movie__meta">
+          <h3 className="movie__title">{title}</h3>
+          <h5 className="movie__year">{year}</h5>
+          <ul className="movie__genres">
+            {genres.map((geners, index) => (
+              <li key={index} className="genres__genre">
+                {geners}
+              </li>))}
+          </ul>
+          <p className="movie__summary">{summary.slice(0, 180)}...</p>
+      </div>
+      </Link>
+    </div>
+    )
+}
+
+Movie.propTpyes = {
+  id : PropTypes.number.isRequired,
+  year : PropTypes.number.isRequired,
+  title : PropTypes.string.isRequired,
+  summary : PropTypes.string.isRequired,
+  poster : PropTypes.string.isRequired,
+  genres : PropTypes.arrayOf(PropTypes.string).isRequired
+}
+
+export default Movie;
---
+++ src/routes/About.js
@@ -0,0 +1,9 @@
+import React from "react"
+import "./About.css"
+
+
+function About(){
+  return <span>About</span>
+}
+
+export default About
